894537c4b3cc95615cc241026a080dfc2b84eb4a
habi-er <kicheol0702@naver.com>
2023-07-05 15:55:24 +0900

    libraryReact

--- libraryReact2/src/contexts/AuthContext.jsx
+++ libraryReact2/src/contexts/AuthContext.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(() => JSON.parse(localStorage.getItem("user")) || null);
+  const [user, setUser] = useState(() => JSON.parse(localStorage.getItem("bookUser")) || null);
   const navigate = useNavigate();
 
   const login = (email, password) => {
